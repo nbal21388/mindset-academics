6fc1de1d3b4df8b24b27cc63739dbe1a0b97527f
Nehaal Khandwalla <nehaalkhandwalla1@gmail.com>
2026-06-14 23:09:45 +0100

    updated main page placeholders

--- app/components/InfoCard.tsx
+++ app/components/InfoCard.tsx
@@ -4,7 +4,7 @@ type InfoCardProps = {
 
 export default function InfoCard({ children }: InfoCardProps) {
     return (
-        <div className="flex flex-1 w-3/4 max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white/20 backdrop-blur-md border border-white/20 rounded-2xl mt-10">
+        <div className="flex flex-1 w-3/4 max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white/20 backdrop-blur-md border border-white/20 rounded-2xl mt-10 mb-5 text-center dark:bg-zinc-900/20 dark:border-zinc-700/40">
             {children}
         </div>
     );
