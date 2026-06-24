66df2aa73abfd35739b307a601dd6c3dcf93dc35
nbal21388 <91699716+nbal21388@users.noreply.github.com>
2026-06-24 23:58:20 +0100

    Add logo to NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavBar() {
     return (
                 <div className="flex flex-row items-center gap-6 text-center sm:items-start sm:text-left z-10">
+                  <Link href="/" className="flex items-center gap-3">
+                  <Image
+                    src="/logo.png"
+                    alt="Logo - Mindset Academics"
+                    width={40}
+                    height={40}
+                  />
+                  <span className="text-blue-400 font-bold text-xl">Mindset Academics</span>
+                </Link>
                   <Link href="/" className="text-pink-300 hover:underline">
                     Home
                   </Link>
