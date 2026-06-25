ae38ad136753e9ca2400c3753116c08048f86d98
nbal21388 <91699716+nbal21388@users.noreply.github.com>
2026-06-25 14:29:49 +0100

    Safeguardingadded

---
+++ app/pages/safeguarding/page.tsx
@@ -0,0 +1,20 @@
+import InfoCard from "@/app/components/InfoCard";
+import NavBar from "@/app/components/NavBar";
+import RippleBackground from "@/app/components/RippleBackground";
+
+export default function safeguarding() {
+    return (
+    <div className="relative z-10 flex flex-col flex-1 items-center justify-center font-sans"> 
+            <NavBar />
+                <h1 className="text-4xl font-bold text-center text-black dark:text-zinc-50 m-6">
+                    Safeguarding
+                </h1>
+            <InfoCard>
+                <p className="mt-6 text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                    Here are our safeguarding policies. If you have any other questions, please don't hesitate to contact us.
+                </p>
+            </InfoCard>
+        </div>
+    );
+}
+                
